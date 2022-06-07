43bd0f918a74dfc779431e3d0f80606d84a12374
kajela <titoyut56@gmail.com>
2022-06-07 01:29:50 -0400

    add the initial files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,22 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
+    static int myInt(int x, int y){
+        return x+ y;
+    }
+    static double myInt(double p, double q){
+        return p*q;
+    }
+    static String myInt(String K,  String l){
+
+        return l;
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(myInt(5,3));
+        System.out.println(myInt(2.11,3.44));
+        System.out.println(myInt("money","lover"));
+
     }
 }
